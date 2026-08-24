efb67e05f6072b6536dc0140b830b2ce24ffd81f
zhli1hao <plm1234567890123123@outlook.com>
2026-08-24 17:52:34 +0800

    Implement OpenGL rendering in MainActivity
    Replaced the main content view with a GLSurfaceView for OpenGL rendering and removed fragment management methods.

--- android/app/src/main/java/com/pubg/pubgforwatch/MainActivity.kt
+++ android/app/src/main/java/com/pubg/pubgforwatch/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.pubg.pubgforwatch
-import android.os.Bundle
+
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(s: Bundle?) {
-        super.onCreate(s)
-        setContentView(R.layout.activity_main)
-        if (s == null) supportFragmentManager.beginTransaction().replace(R.id.container, SplashFragment()).commit()
+    private lateinit var glSurfaceView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        glSurfaceView = GLSurfaceView(this)
+        glSurfaceView.setEGLContextClientVersion(2)
+        glSurfaceView.setRenderer(GameRenderer())
+        setContentView(glSurfaceView)
     }
-    fun openLogin() = switchFrag(LoginFragment())
-    fun openLobby() = switchFrag(LobbyFragment())
-    fun openMatch() = switchFrag(MatchFragment())
-    fun openFriends() = switchFrag(FriendsFragment())
-    fun openChat() = switchFrag(ChatFragment())
-    fun openGame() = switchFrag(GameFragment())
-    fun openSettings() = switchFrag(SettingsFragment())
-    private fun switchFrag(f: Fragment) = supportFragmentManager.beginTransaction().replace(R.id.container, f).addToBackStack(null).commit()
+
+    override fun onPause() {
+        super.onPause()
+        glSurfaceView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glSurfaceView.onResume()
+    }
 }
