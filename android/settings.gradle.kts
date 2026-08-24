6349cd2fd8c4193f107bf4003ca6d844b1106845
zhli1hao <plm1234567890123123@outlook.com>
2026-08-24 16:57:29 +0800

    Delete android/settings.gradle.kts

--- android/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-pluginManagement { repositories { google(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = "pubg-for-watch"; include(":app")
